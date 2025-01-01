b8d25be74b783405892a596fefe707f8baff5b7b
lost <lost.dev.256@gmail.com>
2025-01-01 14:11:01 +0100

    BLN-49 PlaceNode

---
+++ include/Place/PlaceNode.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Babylon::Place
+{
+
+/**
+ * Класс узла размещения
+ */
+class PlaceNode
+{
+public:
+    PlaceNode() = default;
+    virtual ~PlaceNode() = default;
+};
+
+} // namespace Babylon::Place
---
+++ src/Place/PlaceNode.cpp
@@ -0,0 +1,8 @@
+#include <pch.h>
+
+#include <Place/PlaceNode.h>
+
+namespace Babylon::Place
+{
+
+} // namespace Babylon::Place
