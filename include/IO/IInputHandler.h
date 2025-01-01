96d1c5a2762846d2c3d581a35b5f9600efa5ce1b
lost <lost.dev.256@gmail.com>
2025-01-01 13:40:06 +0100

    BLN-32 Интерфейс IInputHandler

---
+++ include/IO/IInputHandler.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Babylon::IO
+{
+
+/**
+ * Интерфейс обработчика системы ввода.
+ */
+class IInputHandler
+{
+public:
+    virtual ~IInputHandler() = default;
+};
+
+} // namespace Babylon::IO
