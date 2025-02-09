7eb7b65112c1643df6af277abac27aa9e0fc04b0
lost <lost.dev.256@gmail.com>
2025-02-09 18:09:51 +0100

    Rename main namespace

--- tools/cmake/cfg.cmake
+++ tools/cmake/cfg.cmake
@@ -3,14 +3,14 @@
 ################################################################################
 cmake_minimum_required(VERSION 3.30.0 FATAL_ERROR)
 
-if(NOT BABYLON_ROOT_DIR)
+if(NOT BN_ROOT_DIR)
     message(FATAL_ERROR "Babylon root directory not found")
 endif()
 
 # Additional build configuration
-function(babylon_unit_external_configure_build UNIT_NAME)
+function(bn_unit_external_configure_build UNIT_NAME)
     if(NOT TARGET ${UNIT_NAME})
-        babylon_log_fatal("Babylon unit (${UNIT_NAME}) doesn't exists")
+        bn_log_fatal("Babylon unit (${UNIT_NAME}) doesn't exists")
         return()
     endif()
 endfunction()
