8608b97dfa94c477a55881a0ff83beb4c3180378
lost <lost.dev.256@gmail.com>
2025-10-01 02:20:09 +0200

    Meson setting (WIP3)

--- tools/generators/gen_mac_xcode.command
+++ tools/generators/gen_mac_xcode.command
@@ -1,2 +1,18 @@
+#!/bin/bash
+
 cd "$(dirname "$0")/../../" || exit
-cmake --preset mac_xcode
+
+BUILDDIR=./build
+
+if [ ! -d "$BUILDDIR" ]; then
+    echo "Creating folder $BUILDDIR..."
+    mkdir "$BUILDDIR"
+fi
+
+if [ -f "$BUILDDIR/meson-info/meson-info.json" ]; then
+    echo "Reconfiguring $BUILDDIR..."
+    meson setup --reconfigure "$BUILDDIR"
+else
+    echo "Configuring $BUILDDIR..."
+    meson setup "$BUILDDIR" . --backend=xcode --native-file tools/meson/presets/babylon.ini --native-file tools/meson/presets/demo.ini
+fi
--- tools/generators/gen_win_vs_msvc_demo.cmd
+++ tools/generators/gen_win_vs_msvc_demo.cmd
@@ -1,16 +1,20 @@
 echo off
+
 cd %~dp0../..
+
 set BUILDDIR=./build
+
 if not exist "%BUILDDIR%" (
     echo Creating folder %BUILDDIR%...
     mkdir "%BUILDDIR%"
 )
+
 if exist %BUILDDIR%/meson-info/meson-info.json (
     echo Reconfiguring %BUILDDIR%...
     meson setup --reconfigure %BUILDDIR%
 ) else (
     echo Configuring %BUILDDIR%...
-    meson setup %BUILDDIR% . --backend=vs --native-file tools/meson/presets/demo.ini
+    meson setup %BUILDDIR% . --backend=vs --native-file tools/meson/presets/babylon.ini --native-file tools/meson/presets/demo.ini
 )
 
 echo Fixing...
